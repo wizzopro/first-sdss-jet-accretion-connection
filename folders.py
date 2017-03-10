efa7c9dfa19c1c87c4d7bdec051677c9095748a0
pieter <poers.science@gmail.com>
2017-03-10 01:48:22 +0100

    Add brief description folders.py

--- folders.py
+++ folders.py
@@ -1,8 +1,10 @@
-home_folder = '/Users/pieter/'
-science_folder= home_folder + 'science/'
-science_origdata_folder = '/Users/pieter/science_origdata/'
+# Setup the folders for your local installation here
 
-firstdata_folder = science_origdata_folder + 'first/archive.stsci.edu/pub/vla_first/data/'
-cutout_folder = home_folder + 'first_cutouts/cutouts/'
-sdss_folder = science_folder + 'sdss/'
-qsodata_folder = sdss_folder + 'qso/dr7/'
+home_folder = '/Users/pieter/'
+science_folder = home_folder + 'science/'  # modified data folder
+science_origdata_folder = '/Users/pieter/science_origdata/'  # unmodified data folder
+
+firstdata_folder = science_origdata_folder + 'first/archive.stsci.edu/pub/vla_first/data/'  # FIRST images folder
+cutout_folder = home_folder + 'first_cutouts/cutouts/'  # cutout folder
+sdss_folder = science_folder + 'sdss/'  # SDSS data folder (modified)
+qsodata_folder = sdss_folder + 'qso/dr7/'  # SDSS DR7 quasar data, Shen et al. (2011), ApJS, 194, 45 (https://arxiv.org/abs/1006.5178) available from http://quasar.astro.illinois.edu/BH_mass/dr7.htm
