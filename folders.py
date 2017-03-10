46111368da2d00f217878f400c52d6eb7d80000c
pieter <poers.science@gmail.com>
2017-03-10 01:21:33 +0100

    Remove .pyc

--- folders.py
+++
@@ -1,8 +0,0 @@
-home_folder = '/Users/pieter/'
-science_folder= home_folder + 'science/'
-science_origdata_folder = '/Users/pieter/science_origdata/'
-
-firstdata_folder = science_origdata_folder + 'first/archive.stsci.edu/pub/vla_first/data/'
-cutout_folder = home_folder + 'first_cutouts/cutouts/'
-sdss_folder = science_folder + 'sdss/'
-qsodata_folder = sdss_folder + 'qso/dr7/'
